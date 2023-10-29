57808e4c0ec2aa7d3637668344556c4ed95f9a1f
Vitaliy Boyarsky <boyarsky.vitaliy@live.com>
2023-10-29 18:04:18 +0200

    Implement lambda function and cover by unit tests

--- src/lambda.py
+++ src/lambda.py
@@ -1,2 +1,87 @@
+import json
+import logging
+import os
+
+import boto3
+
+logging.basicConfig(
+    level=os.environ.get('LOG_LEVEL', 'INFO'),
+    format='%(levelname)-8s %(message)s'
+)
+
+session = boto3.session.Session()
+glue = session.client('glue')
+cloudwatch = session.client('cloudwatch')
+
+tags = []
+if os.environ.get('TAGS'):
+    tags = os.environ['TAGS'].split(',')
+
+
 def handler(event, context):  # pylint: disable=unused-argument
-    pass
+    """
+    Entry point for AWS Lambda
+    """
+
+    logging.info(json.dumps(event))
+    job_run = job_run_details(event)
+    logging.info(job_run)
+
+    dimensions = [
+        {
+            'Name': 'JobName',
+            'Value': job_run['JobName']
+        },
+        *[
+            {
+                'Name': tag,
+                'Value': job_run['Tags'].get(tag, 'Unknown')
+            }
+            for tag in tags
+        ]
+    ]
+
+    metrics_data = [
+        {
+            'MetricName': 'Started',
+            'Dimensions': dimensions,
+            'Timestamp': job_run['StartedOn'],
+            'Value': 1,
+            'Unit': 'Count'
+        },
+        {
+            'MetricName': job_run['JobRunState'].capitalize(),
+            'Dimensions': dimensions,
+            'Timestamp': job_run['CompletedOn'],
+            'Value': 1,
+            'Unit': 'Count'
+        },
+        {
+            'MetricName': 'Duration',
+            'Dimensions': dimensions,
+            'Timestamp': job_run['CompletedOn'],
+            'Value': job_run['ExecutionTime'],
+            'Unit': 'Seconds'
+        }
+    ]
+
+    cloudwatch.put_metric_data(
+        Namespace='Glue',
+        MetricData=metrics_data
+    )
+
+
+def job_run_details(event):
+    """
+    Get details of the job run
+    """
+
+    job_name = event['detail']['jobName']
+    job_arn = f'arn:aws:glue:{event["region"]}:{event["account"]}:job/{job_name}'
+
+    response = glue.get_job_run(JobName=job_name, RunId=event['detail']['jobRunId'])
+    job_run = response['JobRun']
+    response = glue.get_tags(ResourceArn=job_arn)
+    job_run['Tags'] = response['Tags']
+
+    return job_run
--- tests/lambda_test.py
+++ tests/lambda_test.py
@@ -1,2 +1,131 @@
-def test_handler():
-    assert True
+import importlib
+from datetime import datetime
+from unittest import mock
+
+
+@mock.patch("boto3.session.Session")
+def test_handler(mock_session_class):
+    mock_session = mock.Mock()
+    mock_session_class.return_value = mock_session
+    mock_glue = mock.Mock()
+    mock_glue.get_job_run.return_value = create_job_run()
+    mock_glue.get_tags.return_value = create_tags()
+    mock_cloudwatch = mock.Mock()
+    mock_session.client.side_effect = [mock_glue, mock_cloudwatch]
+
+    with mock.patch.dict('os.environ', {'TAGS': 'Environment'}):
+        aws_lambda = importlib.import_module('lambda')
+        aws_lambda.handler(create_event(), mock.Mock())
+
+    mock_glue.get_job_run.assert_called_once_with(
+        JobName='untitled-job',
+        RunId='jr_5d92f3974644a042b9d382c165255fa8618b22708c6c098adc4cdba95553bd9e'
+    )
+    mock_glue.get_tags.assert_called_once_with(
+        ResourceArn='arn:aws:glue:us-east-1:715737992409:job/untitled-job'
+    )
+    assert mock_cloudwatch.put_metric_data.call_args_list == [
+        mock.call(
+            Namespace='Glue',
+            MetricData=[
+                create_metric(
+                    MetricName='Started',
+                    Timestamp=datetime.fromisoformat('2020-09-30T18:29:03Z')),
+                create_metric(
+                    MetricName='Succeeded',
+                    Timestamp=datetime.fromisoformat('2020-09-30T18:31:05Z')),
+                create_metric(
+                    MetricName='Duration',
+                    Timestamp=datetime.fromisoformat('2020-09-30T18:31:05Z'),
+                    Value=122, Unit='Seconds')
+            ]
+        )
+    ]
+
+
+def create_event(**kwargs):
+    return {
+        **{
+            'version': '0',
+            'id': '96e8a234-3c2d-5ce6-3acd-2b8f41ee9110',
+            'detail-type': 'Glue Job State Change',
+            'source': 'aws.glue',
+            'account': '715737992409',
+            'time': '2020-09-30T18:31:07Z',
+            'region': 'us-east-1',
+            'resources': [],
+            'detail': {
+                **{
+                    'jobName': 'untitled-job',
+                    'severity': 'INFO',
+                    'state': 'SUCCEEDED',
+                    'jobRunId': 'jr_5d92f3974644a042b9d382c165255fa8618b22708c6c098adc4cdba95553bd9e',
+                    'message': 'Job run succeeded'
+                },
+                **kwargs.pop('detail', {})
+            }
+        },
+        **kwargs
+    }
+
+
+def create_job_run(**kwargs):
+    return {
+        'JobRun': {
+            **{
+                'Id': 'jr_5d92f3974644a042b9d382c165255fa8618b22708c6c098adc4cdba95553bd9e',
+                'Attempt': 0,
+                'JobName': 'untitled-job',
+                'StartedOn': datetime.fromisoformat('2020-09-30T18:29:03Z'),
+                'LastModifiedOn': datetime.fromisoformat('2020-09-24T12:01:43Z'),
+                'CompletedOn': datetime.fromisoformat('2020-09-30T18:31:05Z'),
+                'JobRunState': 'SUCCEEDED',
+                'PredecessorRuns': [],
+                'AllocatedCapacity': 2,
+                'ExecutionTime': 122,
+                'Timeout': 5,
+                'MaxCapacity': 2.0,
+                'WorkerType': 'G.1X',
+                'NumberOfWorkers': 2,
+                'LogGroupName': '/aws-glue/jobs',
+                'GlueVersion': '4.0',
+                'ExecutionClass': 'STANDARD'
+            },
+            **kwargs
+        }
+    }
+
+
+def create_tags(**kwargs):
+    return {
+        'Tags': {
+            **{
+                'Environment': 'dev',
+                'CostCenter': 'data-platform',
+                'Owner': 'james'
+            },
+            **kwargs
+        }
+    }
+
+
+def create_metric(**kwargs):
+    return {
+        **{
+            'MetricName': 'Started',
+            'Dimensions': [
+                {
+                    'Name': 'JobName',
+                    'Value': 'untitled-job'
+                },
+                {
+                    'Name': 'Environment',
+                    'Value': 'dev'
+                }
+            ],
+            'Timestamp': datetime.fromisoformat('2020-09-30T18:31:05Z'),
+            'Value': 1,
+            'Unit': 'Count'
+        },
+        **kwargs
+    }
